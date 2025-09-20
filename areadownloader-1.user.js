b5c859f1903ea15ff1efb241b457538066a467eb
Demz18 <111712178+Demz18@users.noreply.github.com>
2025-09-19 23:08:20 -0300

    Add files via upload

---
+++ areadownloader-1.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         PixMap Area Downloader
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Area downlaoder for Pixmap.fun
+// @author       demz
+// @icon         https://media.discordapp.net/attachments/1412815236486139996/1412815236737531986/Pixmap_logo_2025_DC.png?ex=68cec239&is=68cd70b9&hm=093395e81a766687f347c082c623217bd321e3c777f72ec24274516fee609563
+// @match        *://pixmap.fun/*
+// @require      https://github.com/Demz18/Pixmap-Area-Download/raw/refs/heads/main/dist/areadownloader.user.js
+// ==/UserScript==
