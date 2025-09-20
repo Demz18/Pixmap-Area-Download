ea5c744f29598bc0e9d2faa2ae5db4213349665f
Demz18 <111712178+Demz18@users.noreply.github.com>
2025-09-20 18:52:52 -0300

    Update areadownloader-1.user.js

--- areadownloader-1.user.js
+++ areadownloader-1.user.js
@@ -1,10 +1,14 @@
 // ==UserScript==
 // @name         PixMap Area Downloader
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Area downlaoder for Pixmap.fun
 // @author       demz
 // @icon         https://media.discordapp.net/attachments/1412815236486139996/1412815236737531986/Pixmap_logo_2025_DC.png?ex=68cec239&is=68cd70b9&hm=093395e81a766687f347c082c623217bd321e3c777f72ec24274516fee609563
 // @match        *://pixmap.fun/*
-// @require      https://github.com/Demz18/Pixmap-Area-Download/raw/refs/heads/main/dist/areadownloader.user.js
+// @updateURL    https://raw.githubusercontent.com/Demz18/Pixmap-Area-Download/main/areadownloader-1.user.js
+// @downloadURL  https://raw.githubusercontent.com/Demz18/Pixmap-Area-Download/main/areadownloader-1.user.js
+// @require      https://raw.githubusercontent.com/Demz18/Pixmap-Area-Download/main/dist/main.user.js
+
+
 // ==/UserScript==
