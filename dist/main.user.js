a2920855f9e8df75317b4c9fcc8e8005821d145d
Demz18 <111712178+Demz18@users.noreply.github.com>
2025-09-20 19:06:40 -0300

    Update main.user.js

--- dist/main.user.js
+++ dist/main.user.js
@@ -7,6 +7,7 @@
 // @match        *://pixmap.fun/*
 // @grant        GM_addStyle
 // @grant        GM_download
+// @icon         https://media.discordapp.net/attachments/1412815236486139996/1412815236737531986/Pixmap_logo_2025_DC.png?ex=68cec239&is=68cd70b9&hm=093395e81a766687f347c082c623217bd321e3c777f72ec24274516fee609563
 // @updateURL    https://raw.githubusercontent.com/Demz18/Pixmap-Area-Download/main/dist/main.user.js
 // @downloadURL  https://raw.githubusercontent.com/Demz18/Pixmap-Area-Download/main/dist/main.user.js
 
